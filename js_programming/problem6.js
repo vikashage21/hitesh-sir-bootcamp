4b55dd60ea1ea22c63afc5a459f8f2b954964015
vikash sharma <vk220783@gmail.com>
2024-07-06 23:33:22 +0530

    solving more problem of js

---
+++ js_programming/problem6.js
@@ -0,0 +1,13 @@
+for (let i=1; i>=1 && i<=25; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}    
+// other solution
+
+for(let i=2 ; i<=25; i++){
+    console.log(i);
+    i+=1;
+    
+    
+}
