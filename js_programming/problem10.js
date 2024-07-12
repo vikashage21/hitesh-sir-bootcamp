df0a5b93da459f141aefc7e8e8a41286086fd4fd
vikash sharma <vk220783@gmail.com>
2024-07-12 18:03:32 +0530

    creating a parallax effect website

--- js_programming/problem10.js
+++ js_programming/problem10.js
@@ -2,8 +2,7 @@
 // function to find the frequency of each character in a string.
 function frequency (str){
     let obj ={}
-    let k =0;
-    let j 
+   
 for (let i=0; i<=str.length-1; i++){
     if(!obj[str[i]]){
         obj[str[i]] = 1
@@ -17,4 +16,4 @@ return obj;
 
 }
 
-console.log(frequency("doll")) // should return {h: 0, e: 1, l: 2, o: 3,  " ": 4, w: 5, r: 6, d: 7}
+console.log(frequency("doll")) // should return {h: 0, e: 1, l: 2, o: 3,  " ": 4, w: 5, r: 6, d: 7} 
