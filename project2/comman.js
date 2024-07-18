558ce1f9eb6c86e75bfc2e857f5687f734aaf2a2
vikash sharma <vk220783@gmail.com>
2024-07-19 00:06:08 +0530

    adding product details page  inside our website

--- project2/comman.js
+++ project2/comman.js
@@ -16,3 +16,40 @@ categoryArr.map((items)=>{
         items.appendChild(category)
     })
 })
+
+
+// fetching dynatic data form api
+
+
+let products = fetch('https://fakestoreapi.com/products').then(res => res.json());
+let productDetails = document.querySelectorAll('.productDetails')
+
+products.then((data)=>{
+    data.map((productItmes)=>{
+        let productCard = document.createElement('div')
+        productCard.classList.add('product-card')
+        
+        let html = `
+        <img src="${productItmes.image}" class='product-image' alt="${productItmes.title}">
+        <div class='productDetails'>
+        <h2>${productItmes.title}</h2>
+        <p>$${productItmes.price}</p>
+        <button class='btn btn-primary m-2'>Buy</button>
+        <button class='btn btn-warning m-2'>Add to Cart</button>
+
+        </div>
+        `;
+        productCard.innerHTML += html;
+        
+        productDetails.forEach((items)=>{
+            items.appendChild(productCard)
+        })
+    })
+ 
+})
+
+
+// gereateing products
+
+
+ 
